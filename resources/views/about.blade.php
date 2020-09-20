cd707ce40aee535852f5146c9ad4d513b422c487
harumimax <harumimax@gmail.com>
2020-09-20 16:45:27 +0300

    add aout

---
+++ resources/views/about.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+       
+        <title>Laravel</title>
+
+
+        <style>
+            body {
+                font-family: 'Arial';
+            }
+        </style>
+    </head>
+
+    <body class="antialiased">
+<h1>О блоге</h1>
+<p>Эксперименты с Laravel на Хекслете</p>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return 'hello, world!';
+});
+
+Route::get('/about', function () {
+    return view('about');
 });
